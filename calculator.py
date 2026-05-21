d294ee061e0a6c424b64bc775241b6e68fc45408
Ritesh <ritssin@gmail.com>
2026-05-21 13:18:27 +0100

    Revert "Add multiplication function"
    This reverts commit a90b12299b5b73efc73d2f9868d1c3912b30abf3.

--- calculator.py
+++ calculator.py
@@ -9,12 +9,7 @@ def subtract(a, b):
     return a - b
 
 
-def multiply(a, b):
-    return a * b
-
-
 if __name__ == "__main__":
     print("Calculator v1")
     print(f"2 + 3 = {add(2, 3)}")
     print(f"10 - 4 = {subtract(10, 4)}")
-    print(f"6 * 7 = {multiply(6, 7)}")
